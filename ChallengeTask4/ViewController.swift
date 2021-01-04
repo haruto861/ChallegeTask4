ed89705dd72b673fc794b0ee9cb0dfc0da4809bf
松島悠人 <msm8610@matsushimayuuninnoMacBook-Air.local>
2021-01-04 17:03:14 +0900

    firstcommit

---
+++ ChallengeTask4/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ChallengeTask4
+//
+//  Created by 松島悠人 on 2021/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBOutlet weak var plusButton: UIButton!
+    
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
+    var num = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        
+        num = num + 1
+        
+        label.text = String(num)
+       
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        
+        num = 0
+        label.text = String(num)
+        
+        
+    }
+    
+}
+
